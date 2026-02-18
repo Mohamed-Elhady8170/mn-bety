89983d0c13244d4fd96d710a4c05847376b3cf77
WessamMohamed22 <wm8821730@gmail.com>
2026-02-18 03:33:15 +0200

    feat: add responsive navbar with mn bety logo

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,7 +1,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cairo': ['Cairo', 'sans-serif'],
+        'display': ['Cairo', 'sans-serif'],
+        'body': ['Cairo', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
