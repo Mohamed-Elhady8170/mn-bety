2da6a18868a104daf67b0e16d04b798466e59e62
WessamMohamed22 <wm8821730@gmail.com>
2026-03-15 03:10:40 +0200

    fix minor issues

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,3 @@
-// import { RouterProvider } from "react-router-dom";
-// import { x } from "./routes/mainLayout";
-// import { Provider } from "react-redux";
-// import store from "./app/store";
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       <RouterProvider router={x} />
-//     </Provider>
-//   );
-// }
-
-// export default App;
-
 import { RouterProvider } from "react-router-dom";
 import { x } from "./routes/mainLayout";
 
