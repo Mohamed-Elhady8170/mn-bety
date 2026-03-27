5db886596a5a801c8943689514f3ebe762810c9d
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-03-28 00:39:50 +0200

    Delete handle cart to local storage from store we handle it with redis

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -21,17 +21,4 @@ export const store = configureStore({
   },
 });
 
-
-// Persist cart state to localStorage whenever cart state changes
-store.subscribe(() => {
-  try {
-    const state = store.getState();
-    // Save only cart items (avoid saving whole state !!!!)
-    localStorage.setItem("mn-bety-cart", JSON.stringify(state.cart.items));
-  } catch (error) {
-    // check for error if may cart is full
-    console.log("Failed to save cart", error);
-  }
-});
-
 export default store;
